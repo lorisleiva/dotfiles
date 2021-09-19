3c7c017a197850aca34212285ba4bae5ebdf6e51
Loris Leiva <loris.leiva@gmail.com>
2021-09-19 22:08:28 +0100

    Add rust path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -6,14 +6,17 @@ export PATH="/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sb
 # Global composer
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 
+# Cargo / Rust
+export PATH="$HOME/.cargo/bin:$PATH"
+
 # VS Code
 export PATH="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin:$PATH"
 
 # RabbitMQ
-export PATH="$PATH:/usr/local/opt/rabbitmq/sbin"
+export PATH="/usr/local/opt/rabbitmq/sbin:$PATH"
 
 # DotNet for Unity
-export PATH="$PATH:/usr/local/share/dotnet"
+export PATH="/usr/local/share/dotnet:$PATH"
 
 # Local bin directories
 export PATH="./bin:./vendor/bin:$PATH"
