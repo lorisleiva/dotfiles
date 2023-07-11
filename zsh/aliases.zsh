acb1eb3e23570aa9dedfa25f1705d52276e63994
Loris Leiva <loris.leiva@gmail.com>
2023-07-11 13:36:22 +0100

    Add Metaplex aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -76,3 +76,7 @@ alias path='echo -e ${PATH//:/\\n}'
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias iplocal="ifconfig | grep 'inet ' | grep -Fv 127.0.0.1 | awk '{print \$2}'"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Metaplex
+alias mplp="pnpm programs:build && pnpm generate && pnpm validator"
+alias mplc="pnpm build && pnpm test"
