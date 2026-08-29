0c8e1e517b4307c621996f3d451d9953ef213205
Loris Leiva <loris.leiva@gmail.com>
2026-08-29 13:43:56 +0100

    Update homebrew.nix

--- homebrew.nix
+++ homebrew.nix
@@ -17,7 +17,6 @@
       "anomalyco/tap"
       "blendle/blendle"
       "shivammathur/php"
-      "steipete/tap"
       "withgraphite/tap"
     ];
 
@@ -100,8 +99,6 @@
       "vlc"
       "wezterm"
       "whatsapp"
-      # tapped casks (full paths)
-      "steipete/tap/codexbar"
     ];
 
     # NOTE: Mac App Store apps are intentionally not declared here.
